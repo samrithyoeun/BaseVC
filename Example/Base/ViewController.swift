3d6884cdeef9e47a470c61380c6b5f9b3a8ef06d
Samrith Yoeun <samrith.yoe@ascendcorp.com>
2019-12-10 20:20:55 +0700

    test new file

--- Example/Base/ViewController.swift
+++ Example/Base/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Base
 
 class ViewController: UIViewController {
 
---
+++ Testing.swift
@@ -0,0 +1,14 @@
+//
+//  Testing.swift
+//  Base
+//
+//  Created by Yoeun Samrith on 12/10/19.
+//
+
+import Foundation
+
+class Testing {
+    func foo() {
+        print("foooooooooo")
+    }
+}
